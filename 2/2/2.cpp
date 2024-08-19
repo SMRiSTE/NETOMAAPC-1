d42b637f50df3aaefa876e8cae0ee4268f1bc5fe
SMRist <s.m.r.00800ist@gmail.com>
2024-08-20 01:16:50 +0300

    Initial commit

--- 2/2/2.cpp
+++ 2/2/2.cpp
@@ -2,6 +2,7 @@
 #include<thread>
 #include<vector>
 #include<chrono>
+#include<iomanip>
 
 void sumVec(std::vector<int> &vec1, std::vector <int> &vec2, std::vector<int> &result, int start, int end) {
     for (int i = start; i < end; ++i) {
@@ -16,6 +17,8 @@ int main() {
 
     int num_threads[5] = {1, 2, 4, 8, 16 };
     std::vector <int> num_elements = { 1000, 10000, 100000, 1000000 };
+    int var = 0;
+    std::vector<double> res = {};
 
     std::cout << "Количество аппаратных ядер - " << std::thread::hardware_concurrency() << std::endl;
 
@@ -41,8 +44,29 @@ int main() {
             auto end_time = std::chrono::high_resolution_clock::now();
             std::chrono::duration<double> elapsed = end_time - start_time;
 
-            std::cout << num_threads[j] << " поток, " << num_elements[i] << " элементов: " << elapsed.count() << " секунд" << std::endl;
+            //std::cout << num_threads[j] << " поток, " << num_elements[i] << " элементов: " << elapsed.count() << " секунд" << std::endl;
+
+            res.push_back(elapsed.count());
+
         }
+        
+    }
+
+    std::cout << std::setw(25) << num_elements[0];
+    for (int i = 1; i < 4; i++) {
+        std::cout << std::setw(15) << num_elements[i];
+    }
+
+    std::cout << std::endl;
+
+    for (int i = 0; i < 5; i++) {
+        var = i;
+        std::cout << num_threads[i] << " потоков ";
+        for (int j = 0; j < 4; j++) {
+            std::cout << std::setw(15) << res[var] << " ";
+            var+=5;
+        }
+        std::cout << std::endl;
     }
 
     return 0;
